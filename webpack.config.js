ccf60db2d1abb65e260769d6d96869b3cd0134af
chandu <pbommishetti@codestak.com>
2022-04-07 16:34:33 +0530

    demo commit with obfuscation

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 // webpack.config.js
-const JavaScriptObfuscator = require('webpack-obfuscator');
+const JavaScriptObfuscator = require("webpack-obfuscator");
 module.exports = (config, options) => {
-  if (config.mode === 'production') {
-    config.plugins.push(new JavaScriptObfuscator({
-      rotateStringArray: true, // please customizable with options
-    }, ['exclude_bundle.js']));
+  if (config.mode === "production") {
+    config.plugins.push(
+      new JavaScriptObfuscator(
+        {
+          rotateStringArray: true, // please customizable with options
+          compact: true,
+          controlFlowFlattening: false,
+          deadCodeInjection: false,
+          debugProtection: false,
+          // debugProtectionInterval: false,
+          disableConsoleOutput: true,
+          identifierNamesGenerator: "hexadecimal",
+          log: false,
+          renameGlobals: false,
+          selfDefending: true,
+          shuffleStringArray: true,
+          splitStrings: false,
+          stringArray: true,
+          // stringArrayEncoding: false,
+          stringArrayThreshold: 0.75,
+          unicodeEscapeSequence: false,
+        },
+        ["exclude_bundle.js"]
+      )
+    );
   }
-}
+};
